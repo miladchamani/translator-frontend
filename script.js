79b230ba487aef5ec8354edf12f93a22f67486b5
miladchamani <milad.chamani81@gmail.com>
2025-08-21 12:21:03 +0330

    Fix API URL

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ async function validatePassword() {
 async function startTranslation() {
     if (!currentFile || originalLines.length === 0) return;
     // CORRECTED 'if' CONDITION
-	   if (!API_BASE_URL || API_BASE_URL === 'YOUR_WORKER_URL') {
+	   if (!API_BASE_URL || API_BASE_URL === 'https://subtitle-translator.milad-ch-1981.workers.dev') {
 		alert("Error: API_BASE_URL is not set in script.js! Please replace the placeholder with your actual worker URL.");
 		return;
 	  }
